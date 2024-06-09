bfb87cfb88b1d31f3bae13e9123f77d578a77daa
KanekiKen1111 <142958818+KanekiKen1111@users.noreply.github.com>
2024-06-09 21:54:46 +0800

    5
    5

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -9,23 +9,36 @@ public class Health : MonoBehaviour
     [SerializeField] private float initialHealth = 10f;
     [SerializeField] private float maxHealth = 10f;
 
+    [Header("Shield")] 
+    [SerializeField] private float initialShield = 5f;
+    [SerializeField] private float maxShield = 5f;
+
     [Header("Settings")] 
     [SerializeField] private bool destroyObject;
 
     private Character character;
+    private CharacterController controller;
     private Collider2D collider2D;
     private SpriteRenderer spriteRenderer;
 
+    private bool shieldBroken;
+
     // Controls the current health of the object    
     public float CurrentHealth { get; set; }
+
+    // Returns the current shield of this character
+    public float CurrentShield { get; set; }
     
     private void Awake()
     {
         character = GetComponent<Character>();
+        controller = GetComponent<CharacterController>();
         collider2D = GetComponent<Collider2D>();        
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
 
-        CurrentHealth = initialHealth;        
+        CurrentHealth = initialHealth;
+        CurrentShield = initialShield;
+        UIManager.Instance.UpdateHealth(CurrentHealth, maxHealth, CurrentShield, maxShield);        
     }
 
     private void Update()
@@ -43,8 +56,21 @@ public class Health : MonoBehaviour
         {
             return;
         }
+
+        if (!shieldBroken && character != null)
+        {
+            CurrentShield -= damage;
+            UIManager.Instance.UpdateHealth(CurrentHealth, maxHealth, CurrentShield, maxShield);
+
+            if (CurrentShield <= 0)
+            {
+                shieldBroken = true;
+            }
+            return;
+        }
         
-        CurrentHealth -= damage;     
+        CurrentHealth -= damage;
+        UIManager.Instance.UpdateHealth(CurrentHealth, maxHealth, CurrentShield, maxShield);
 
         if (CurrentHealth <= 0)
         {
@@ -59,6 +85,9 @@ public class Health : MonoBehaviour
         {
             collider2D.enabled = false;
             spriteRenderer.enabled = false;
+
+            character.enabled = false;
+            controller.enabled = false;
         }
 
         if (destroyObject)
@@ -69,8 +98,24 @@ public class Health : MonoBehaviour
     
     // Revive this game object    
     public void Revive()
-{
+    {
+        if (character != null)
+        {
+            collider2D.enabled = true;
+            spriteRenderer.enabled = true;
 
+            character.enabled = true;
+            controller.enabled = true;
+        }
+
+        gameObject.SetActive(true);
+
+        CurrentHealth = initialHealth;
+        CurrentShield = initialShield;
+
+        shieldBroken = false;
+
+        UIManager.Instance.UpdateHealth(CurrentHealth, maxHealth, CurrentShield, maxShield);
 }
 
     // If destroyObject is selected, we destroy this game object
