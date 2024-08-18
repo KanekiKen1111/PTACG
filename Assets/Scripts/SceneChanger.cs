f6463f258a0b9a714afa8255bb5c8b2f83a0fdfc
KanekiKen1111 <142958818+KanekiKen1111@users.noreply.github.com>
2024-08-19 01:18:01 +0800

    36

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    // This method changes to the specified scene.
+    // Call this method and pass the name of the scene you want to load.
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Example of changing scenes based on a collision with a GameObject
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Change to a scene named "NextScene"
+            ChangeScene("Village Scene");
+        }
+    }
+}
