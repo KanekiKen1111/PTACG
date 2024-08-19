d32a209a77d85760e5d3c594a0c33cf89ea47fba
Rohii19 <142196054+Rohii19@users.noreply.github.com>
2024-08-19 13:35:11 +0800

    scene changing collision done

--- Assets/Scripts/Misc/ChangeSceneCollision.cs
+++ Assets/Scripts/Misc/ChangeSceneCollision.cs
@@ -8,10 +8,17 @@ public class ChangeSceneOnCollision : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("Collision detected with: " + other.gameObject.name);
+
         if (other.CompareTag("Player"))
         {
+            Debug.Log("Player detected. Changing scene to: " + sceneToLoad);
             // Change the scene when the player collides with the object
             SceneManager.LoadScene(sceneToLoad);
+        }
+        else
+        {
+            Debug.Log("Non-player object collided: " + other.gameObject.name);
         }
     }
 }
