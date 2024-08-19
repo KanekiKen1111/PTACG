765755dc684616a151e161b0a86e96b2cc887375
Rohii19 <142196054+Rohii19@users.noreply.github.com>
2024-08-19 11:55:28 +0800

    final update

--- Assets/Animation/door/SwitchController.cs
+++ Assets/Animation/door/SwitchController.cs
@@ -4,11 +4,21 @@ public class SwitchController : MonoBehaviour
 {
     public DoorController doorController;
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             doorController.SetDoorState(true);
         }
     }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        // Reset the flag when the object exits the trigger
+        if (other.CompareTag("Player"))
+        {
+            doorController.SetDoorState(false);
+        }
+    }
+
+
 }
